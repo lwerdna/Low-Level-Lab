e689d354d414fc3472becfa61dfaf1f8377fc208
Andrew Lamoureux <andrew@vector35.com>
2019-09-11 00:45:16 -0400

    Update genrsrc.py to python 3

--- genrsrc.py
+++ genrsrc.py
@@ -16,13 +16,13 @@ for root, dirs, files in os.walk(pathResources):
         arrname = 'rsrc_' + re.sub(r'[^\w]', '_', fname);
 
         if sys.argv[1:] and sys.argv[1] == 'header':
-            print '// from file %s' % fpath
-            print 'extern unsigned char %s[%d];' % (arrname, len(stuff))
+            print('// from file %s' % fpath)
+            print('extern unsigned char %s[%d];' % (arrname, len(stuff)))
         elif sys.argv[1:] and sys.argv[1] == 'source':
-            print '// from file %s' % fpath
-            print 'unsigned char %s[%d] = { ' % (arrname, len(stuff))
-            print ','.join(map(lambda x: '0x%X' % ord(x), list(stuff)))
-            print ' };\n'
+            print('// from file %s' % fpath)
+            print('unsigned char %s[%d] = { ' % (arrname, len(stuff)))
+            print(','.join(map(lambda x: '0x%X' % x, list(stuff))))
+            print(' };\n')
         else:
             raise "ERROR: need argument \"header\" or \"source\""
 
